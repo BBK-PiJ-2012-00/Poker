93d38acf176ed877b07897cbcdf30702a6d65f9e
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-03-18 00:50:54 +0000

    Thoroughly tested Card toString() method.

--- tests/CardTest.java
+++ tests/CardTest.java
@@ -34,5 +34,43 @@ public class CardTest {
 		
 		assertEquals("Problem with getRank()", expectedRank, outputRank);		
 	}
+	
+	@Test
+	public void test3() {
+		Card card = new CardImpl(1,"D");
+		String output = card.toString();
+		String expected = "[AD]";
+		
+		assertEquals("Problem with toString()", expected, output);
+	}
+	
+	@Test
+	public void test4() {
+		Card card = new CardImpl(11,"S");
+		String output = card.toString();
+		String expected = "[JS]";
+		
+		assertEquals("Problem with toString()", expected, output);
+	}
+	
+	@Test
+	public void test5() {
+		Card card = new CardImpl(12,"H");
+		String output = card.toString();
+		String expected = "[QH]";
+		
+		assertEquals("Problem with toString()", expected, output);
+	}
+	
+	@Test 
+	public void test6() {
+		Card card = new CardImpl(13,"C");
+		String output = card.toString();
+		String expected = "[KC]";
+		
+		assertEquals("Problem with toString()", expected, output);
+	}
+	
+	//Don't forget to test comparing cards to one another once class implements Comparable
 
 }
