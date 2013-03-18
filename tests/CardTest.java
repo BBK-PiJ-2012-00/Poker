4ca996b8c6ad81fd7909a98689f062fa339cb4a4
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-03-18 00:39:57 +0000

    Tests on Card getters successful.

--- tests/CardTest.java
+++ tests/CardTest.java
@@ -20,16 +20,19 @@ public class CardTest {
 	@Test
 	public void test() {
 		String expectedSuit = "D";
-		int expectedRank = 1;
-		Card card = new CardImpl(1, "D");
-		String outputSuit = "F";
+		Card card = new CardImpl(1, expectedSuit);
+		String outputSuit = card.getSuit();
 		
 		assertEquals("Problem with getSuit()", expectedSuit, outputSuit);		
 	}
 	
 	@Test
 	public void test2() {
+		int expectedRank = 1;
+		Card card = new CardImpl(expectedRank, "D");
+		int outputRank = card.getRank();
 		
+		assertEquals("Problem with getRank()", expectedRank, outputRank);		
 	}
 
 }
