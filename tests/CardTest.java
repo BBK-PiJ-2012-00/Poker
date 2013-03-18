3e712963e5a5898783ab5e01215bb5607106af1e
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-03-18 00:35:13 +0000

    Performing tests on Card class.

--- tests/CardTest.java
+++ tests/CardTest.java
@@ -5,6 +5,7 @@ package tests;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 import code.Card;
@@ -15,10 +16,20 @@ import code.CardImpl;
  *
  */
 public class CardTest {
-
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		String expectedSuit = "D";
+		int expectedRank = 1;
+		Card card = new CardImpl(1, "D");
+		String outputSuit = "F";
+		
+		assertEquals("Problem with getSuit()", expectedSuit, outputSuit);		
+	}
+	
+	@Test
+	public void test2() {
+		
 	}
 
 }
