62d66e07fb1093d9f089a6539560a72c91ec0eb6
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-03-22 14:40:06 +0000

    Done some initial testing on Hand.

--- tests/HandTest.java
+++ tests/HandTest.java
@@ -5,22 +5,45 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import code.Hand;
 import code.FiveCardHand;
 import code.Card;
 import code.CardImpl;
+import code.Deck;
+import code.DeckImpl;
 
 /**
  * @author Anna Taylor
  *
  */
 public class HandTest {
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetContents() {
+		Hand testHand = new FiveCardHand();
+		Card[] contents = testHand.getContents();
+		assertNotNull(contents);
+		
+	}
+	
+	@Test
+	public void testGetContents2() {
+		Hand testHand = new FiveCardHand();
+		Card[] contents = testHand.getContents();
+		int expectedSize = 5;
+		int actualSize = contents.length;
+		assertEquals("Problem with handContents initialisation", expectedSize, actualSize);
+	}
+	
+	@Test
+	public void testGetContents3()	{
+		Hand testHand = new FiveCardHand();
+		Card[] contents = testHand.getContents();
+		assertNull(contents[0]); //handContents should be an empty array upon creation.
 	}
 
 }
