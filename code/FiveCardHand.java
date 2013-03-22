90b7227f7fe87cfe163f202944ea0ce160365ceb
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-03-22 13:31:36 +0000

    Added Hand interface and FiveCardHand implementation.

---
+++ code/FiveCardHand.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package code;
+
+/**
+ * @author Anna Taylor
+ *
+ */
+public class FiveCardHand implements Hand {
+	private static final int SIZE = 5;
+	private Card[] handContents = new CardImpl[SIZE];
+	
+	
+	/* (non-Javadoc)
+	 * @see code.Hand#getContents()
+	 */
+	@Override
+	public Card[] getContents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ code/Hand.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package code;
+
+/**
+ * @author Anna Taylor
+ *
+ */
+public interface Hand {
+	
+	/**
+	 * Accessor method to access the cards that make up a hand.
+	 * 
+	 * @return an array of cards that constitutes the hand.
+	 */
+	public Card[] getContents();
+	
+
+}
