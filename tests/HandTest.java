eccb1cf792c69b3795071e163a19394d93af3014
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-03-22 13:39:45 +0000

    Testing initial version of Hand.

---
+++ tests/HandTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.Hand;
+import code.FiveCardHand;
+import code.Card;
+import code.CardImpl;
+
+/**
+ * @author Anna Taylor
+ *
+ */
+public class HandTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
