8a9ed3b9484a0f52c89c58e99db5a093431b066c
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-03-22 16:04:48 +0000

    Fleshing out Hand class.

--- code/FiveCardHand.java
+++ code/FiveCardHand.java
@@ -3,14 +3,21 @@
  */
 package code;
 
+import java.util.Arrays;
+
 /**
  * @author Anna Taylor
  *
  */
 public class FiveCardHand implements Hand {
+	private String handValue; //Should hand values be enums? 
+	
 	private static final int SIZE = 5;
 	private Card[] handContents = new CardImpl[SIZE];
-	private int nextIndex = 0;
+	private int availableIndex = 0;
+	
+	private IllegalArgumentException illegalArgEx = new IllegalArgumentException();
+	private ArrayIndexOutOfBoundsException boundsEx = new ArrayIndexOutOfBoundsException();
 	
 	
 	@Override
@@ -21,8 +28,65 @@ public class FiveCardHand implements Hand {
 	
 	@Override
 	public void addCard(Card c) {
+		try {
+			if (availableIndex < 5) {
+				handContents[availableIndex] = c;
+				availableIndex++;
+			}
+			else {
+				throw boundsEx;
+			}						
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("(!) Attempting to add more cards to a hand than is allowed.");
+		}		
+	}
+
+
+	@Override
+	public void discardCard(Card c) {
+		try {
+			if (availableIndex > 0) {
+				for (int i = 0; i < handContents.length; i++) {
+					if (handContents[i] == c) {
+						handContents[i] = null;
+						this.sort();//Re-sort the hand after card removal, leaving last index(es) free
+						availableIndex--;
+						return; //If found, remove and exit the method;
+					}
+				}
+				throw illegalArgEx;//If this line is reached, no matching card was found (so it's not in the array)
+			}
+			else {
+				throw illegalArgEx;//If the array is empty, a card cannot be removed from it
+			}
+		}
+		catch (IllegalArgumentException e) {
+			
+		}
+		
+	}
+
+	
+	
+	public void sort() {
 		// TODO Auto-generated method stub
 		
+	}
+
+
+	
+	@Override
+	public void evaluateHand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public String getHandValue() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- code/Hand.java
+++ code/Hand.java
@@ -22,5 +22,33 @@ public interface Hand {
 	 * @param Card the card to be added to the hand.
 	 */
 	public void addCard(Card c);
+	
+	/**
+	 * Allows for the removal of a card should the player wish to draw
+	 * another from the deck.
+	 * 
+	 * @param Card the card to be removed from the hand.
+	 */
+	public void discardCard(Card c);
+	
+	/**
+	 * Sorts the cards in the hand by rank.
+	 */
+	public void sort();
+	
+	/**
+	 * Evaluates the value of a hand; whether it is a Flush, Straight etc, 
+	 * or nothing.  Sets the handValue field of Hand to hold this value.
+	 */
+	public void evaluateHand();
+	
+	/**
+	 * Returns the value of the hand e.g. Flush, Straight etc
+	 * 
+	 * @return the evaluation of the hand as a String.
+	 */
+	public String getHandValue();
+	
+
 
 }
