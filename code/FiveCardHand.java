507b68cb3be7e04cd4f53065905711c91f3dd466
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-03-26 14:03:22 +0000

    Ace now has value of 14 as opposed to 1 (this makes things easier when evaluating hands as Ace is high by default). Implementing testing for Straight in evaluateHand().

--- code/FiveCardHand.java
+++ code/FiveCardHand.java
@@ -5,6 +5,7 @@ package code;
 
 import java.util.Arrays;
 
+
 /**
  * @author Anna Taylor
  *
@@ -102,20 +103,50 @@ public class FiveCardHand implements Hand {
 			return; //Don't test for any other handValues if Four of a Kind is found
 		}
 		
-		else { //Test for flush
-			boolean flush = true;
-			for (int i = 0; i < 3; i++) {
-				if (handContents[i].getSuit() != handContents[i+1].getSuit()) {
-					flush = false;
-					break; //Stop testing for flush as soon as 
+		//Test for flush (only reachable if fourOfAKind is false)
+		boolean flush = true;
+		for (int i = 0; i < 3; i++) {
+			if (handContents[i].getSuit() != handContents[i+1].getSuit()) {
+				flush = false;
+				break; //Stop testing for flush as soon as 
+			}
+		}
+		if (flush) {
+			handValue = "Flush";
+			return; //Don't test for any other handValues if a Flush is found
+		}
+		
+		//Test for Straight (only reachable if fourOfAKind and flush are false)
+		boolean straight = true;
+		//Test first for the case where Ace can be low 
+		if ((handContents[4].getRankValue() == 14) && (handContents[3].getRankValue() == 5)) { 
+			//If the highest card in the hand is Ace and a 5 is preceding, test for straight where Ace would be low
+			for (int i = 2; i > -1; i--) {
+				if (handContents[i].getRankValue() != (handContents[i+1].getRankValue() - 1)) {
+					straight = false;
 				}
 			}
-			if (flush) {
-				handValue = "Flush";
-				return; //Don't test for any other handValues if a Flush is found
-			}
-			
 		}
+		if (straight) {
+			handValue = "Straight";
+			return;
+		}			
+	
+		//All other possible straight hands tested next (unreachable if another hand has already been found)
+		for (int i = 0; i < 3; i++) {
+			if (handContents[i].getRankValue() != (handContents[i+1].getRankValue() - 1)) {
+				straight = false;
+			}
+		}
+		if (straight) {
+			handValue = "Straight";
+			return;
+		}
+		
+			
+		
+		
+		
 		
 	}
 
